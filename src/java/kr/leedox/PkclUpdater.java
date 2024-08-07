4750726146943395c056f1976db12e1552b2038e
LEEDOX <leedox@naver.com>
2024-08-07 12:42:28 +0900

    PKCL

--- src/java/kr/leedox/PkclUpdater.java
+++ src/java/kr/leedox/PkclUpdater.java
@@ -12,13 +12,15 @@ public class PkclUpdater {
 
     private static SessionFactory sessionFactory;
 
-    public static void init() {
+    public static void init(String db) {
         try {
-            Configuration config = new Configuration().configure("hibernate.cfg.tibero.xml");
+            String cfg = String.format("hibernate.cfg.%s.xml", db);
+            Configuration config = new Configuration().configure(cfg);
             config.setNamingStrategy(new CustomNamingStrategy());
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(config.getProperties())
+                    .build();
             sessionFactory = config.buildSessionFactory(serviceRegistry);
-        } catch(Throwable ex) {
+        } catch (Throwable ex) {
             System.err.println(ex.getMessage());
             ex.printStackTrace();
         }
@@ -28,20 +30,20 @@ public class PkclUpdater {
         return sessionFactory.openSession();
     }
 
-
     public static void main(String[] args) {
 
-        if(args.length < 2) {
-            System.err.println("Usage: PkclUpdater MDIE CMPK");
+        if (args.length < 3) {
+            System.err.println("Usage: PkclUpdater sqlite MDIE CMPK");
             System.exit(0);
         }
 
-        String come = args[0];
-        String pkgt = args[1];
+        String db = args[0];
+        String come = args[1];
+        String pkgt = args[2];
 
         Object[] pkcls = PkclDataProvider.readPkclData(come, pkgt);
 
-        init();
+        init(db);
         Session session = getSession();
 
         int chk = 0;
@@ -51,7 +53,7 @@ public class PkclUpdater {
         System.out.println(">>> Checking ===> " + come + " " + pkgt);
         System.out.println("=================================================");
 
-        for(int i = 0 ; i < pkcls.length ; i++) {
+        for (int i = 0; i < pkcls.length; i++) {
             Object[] pkclData = (Object[]) pkcls[i];
             String key1 = (String) pkclData[0];
             String key2 = (String) pkclData[1];
@@ -63,13 +65,13 @@ public class PkclUpdater {
             PkclPK pkclPK = new PkclPK(key1, key2, key3, key4);
             Pkcl pkcl = (Pkcl) session.get(Pkcl.class, pkclPK);
 
-            if(pkcl == null) {
+            if (pkcl == null) {
                 continue;
             }
 
             chk++;
 
-            if(pkcl.getPkclWithScrn() != scrnWidth || !align.equals(pkcl.getPkclColmAlgn())) {
+            if (pkcl.getPkclWithScrn() != scrnWidth || !align.equals(pkcl.getPkclColmAlgn())) {
                 System.out.println("-------------------------------------------------");
                 showPkcl("CHK", pkcl);
                 Transaction tx = session.beginTransaction();
@@ -88,10 +90,14 @@ public class PkclUpdater {
         System.out.println("=================================================");
         System.out.println(String.format(">>> STAT ===> CHK: %2d, UPD: %2d", chk, upd));
         System.out.println("=================================================");
+
+        sessionFactory.close();
+        System.exit(0);
     }
 
     private static void showPkcl(String chk, Pkcl pkcl) {
-        String msg = String.format("%s: %2d %-15s ==> %s %3d %s",chk ,pkcl.getPkclSortNumb(), pkcl.getPkclPK().getKey3(), pkcl.getPkclColmAlgn(), pkcl.getPkclWithScrn(), pkcl.getPkclUpdtDate());
+        String msg = String.format("%s: %2d %-15s ==> %s %3d %s", chk, pkcl.getPkclSortNumb(),
+                pkcl.getPkclPK().getKey3(), pkcl.getPkclColmAlgn(), pkcl.getPkclWithScrn(), pkcl.getPkclUpdtDate());
         System.out.println(msg);
     }
 }
