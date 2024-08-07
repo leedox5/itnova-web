82c7314584edadb2f5800639e9c4dd210bdb75e5
LEEDOX <leedox@naver.com>
2024-08-07 10:57:46 +0900

    PKCL

--- src/java/kr/leedox/Pkcl.java
+++ src/java/kr/leedox/Pkcl.java
@@ -1,55 +1,21 @@
 package kr.leedox;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Pkcl {
     private PkclPK pkclPK;
-
     private int pkclSortNumb;
     private String pkclColmAlgn;
     private int pkclWithScrn;
     private String pkclUpdtDate;
-
-    public Pkcl() {
-        super();
-    }
-
-    public PkclPK getPkclPK() {
-        return pkclPK;
-    }
-
-    public void setPkclPK(PkclPK pkclPK) {
-        this.pkclPK = pkclPK;
-    }
-
-    public int getPkclWithScrn() {
-        return pkclWithScrn;
-    }
-
-    public void setPkclWithScrn(int pkclWithScrn) {
-        this.pkclWithScrn = pkclWithScrn;
-    }
-
-    public String getPkclColmAlgn() {
-        return pkclColmAlgn;
-    }
-
-    public void setPkclColmAlgn(String pkclColmAlgn) {
-        this.pkclColmAlgn = pkclColmAlgn;
-    }
-
-    public String getPkclUpdtDate() {
-        return pkclUpdtDate;
-    }
-
-    public void setPkclUpdtDate(String pkclUpdtDate) {
-        this.pkclUpdtDate = pkclUpdtDate;
-    }
-
-    public int getPkclSortNumb() {
-        return pkclSortNumb;
-    }
-
-    public void setPkclSortNumb(int pkclSortNumb) {
-        this.pkclSortNumb = pkclSortNumb;
-    }
-
+    private String pkclUpdtCman;
 }
